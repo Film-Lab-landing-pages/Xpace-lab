05bf39888964453372815c379af0b416f97950d9
Diego Sanchez <diegosmtattoo@gmail.com>
2025-05-22 19:10:29 -0300

    feat: about section toggle layout

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -2,25 +2,34 @@
 import React from "react";
 
 import { Section } from "../../globalStyles";
-import { AboutCard, Contact, ToggleButton } from "./styles";
+import {
+  AboutButton,
+  AboutCard,
+  Contact,
+  OurMissionButton,
+  OurMissionCard,
+} from "./styles";
 
 const About: React.FC = () => {
+  const [ourMissionActive, setOurMissionActive] = React.useState(false);
+
   const pink = "#F90096";
   const yellow = "#F9B807";
   const blue = "#5273FF";
   return (
     <Section backgroundheight="60%">
       <img
+        id="about"
         src="images/earth.png"
         alt="Terra"
-        className="planet-rotation absolute left-0 top-1/3 -translate-y-1/4 -translate-x-1/2 h-[45%] max-h-[510px] object-contain"
+        className="planet-rotation  scroll-mt-[500px] absolute left-0 top-1/3 -translate-y-1/4 -translate-x-1/2 h-[45%] max-h-[510px] object-contain"
       />
       <img
         src="images/moon.png"
         alt="Lua"
-        className="planet-rotation absolute bottom-0 left-1/2 translate-y-[800px] -translate-x-1/2 h-[1280px] object-contain"
+        className="planet-rotation absolute bottom-0 left-1/2 translate-y-[70%] -translate-x-1/2 h-[100vh] min-h-[700px] max-h-[1400px] object-contain"
       />
-      <AboutCard>
+      <AboutCard className={ourMissionActive ? "active" : ""}>
         <img
           className="display-top"
           src="./images/about-top.png"
@@ -29,24 +38,41 @@ const About: React.FC = () => {
         <h2>Sobre Nós</h2>
         <p>
           No centro de uma galáxia criativa, pulsa o
-          <span style={{ fontWeight: "700", color: "#F90094" }}>Xpace Lab</span>
+          <span className="pink-span"> Xpace Lab</span>
           — um universo onde <br /> estúdios, agências, escolas e ideias orbitam
           em perfeita sincronia. Aqui, ,<br /> conectamos a arte ao mercado, a
           inovação ao impacto, e o digital ao humano. <br />
-          <span style={{ fontWeight: "700", color: "#FFB800" }}>
+          <span className="yellow-span">
             Seja bem-vindo ao espaço onde a criatividade expande fronteiras.
           </span>
         </p>
-        <ToggleButton buttoncolor={yellow}>
-          <h2>Nosso Propósito</h2>
-        </ToggleButton>
+        <OurMissionButton
+          buttoncolor={yellow}
+          onClick={() => setOurMissionActive(true)}
+        >
+          <h2>NOSSO PROPÓSITO</h2>
+        </OurMissionButton>
         <img
           className="display-bottom"
           src="./images/about2.png"
           alt="Elemento visual da sessão 'Sobre Nós' "
         />
       </AboutCard>
-      <Contact>
+      <OurMissionCard className={!ourMissionActive ? "active" : ""}>
+        <AboutButton
+          buttoncolor={pink}
+          onClick={() => setOurMissionActive(false)}
+        >
+          <h2>SOBRE NÓS</h2>
+        </AboutButton>
+        <h2>Nosso Propósito</h2>
+        <p>
+          Acreditamos em criar pontes entre o mercado e a cultura, entre o que é
+          técnico e o que é sensível, entre o que comunica e o que transforma.
+        </p>
+      </OurMissionCard>
+
+      <Contact className="scroll-mt-[1200px]">
         <h2>Contato</h2>
         <a>
           <img
